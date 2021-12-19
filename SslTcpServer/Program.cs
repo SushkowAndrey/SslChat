e772a0fa5ce651192fedaee1907e6f60e90e5eb3
Starinin Andrey <starininandrey@gmail.com>
2021-12-19 12:54:36 +0300

    Init Server

---
+++ SslTcpServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SslTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
